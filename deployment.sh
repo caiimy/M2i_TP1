e26fbef345b5f71a28eb2ac86c1ad2ce94671203
houssamD <houssam.darrab@labom2iformation.fr>
2023-10-18 17:06:06 +0200

    commit updat first test

--- deployment.sh
+++ deployment.sh
@@ -3,7 +3,9 @@
 # Vérifier si Terraform est installé
 if ! [ -x "$(command -v terraform)" ]; then
   echo "Installation Terraform..."
-  sudo apt install terraform -y
+  wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+  sudo apt update && sudo apt install terraform
 fi
 
 # initialiser le dossier Terraform
