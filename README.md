82c96fbec13d4ebaf0091ef896051d2d609bc9c6
houssamD <houssam.darrab@labom2iformation.fr>
2023-10-18 14:28:25 +0200

    commit first test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Foot2rue !
+TP1 : Créer des VM sur GCP à l’aide de terraform et déployer une application Wordpress dans ces VM avec Ansible
+
+Documentation
--- deployment.sh
+++ deployment.sh
@@ -2,28 +2,34 @@
 
 # Vérifier si Terraform est installé
 if ! [ -x "$(command -v terraform)" ]; then
-  echo "Installing Terraform..."
+  echo "Installation Terraform..."
   sudo apt install terraform -y
 fi
 
 # initialiser le dossier Terraform
-terraform init
+if ! [ -f .terraform ]; then
+  echo "Initialisation du terraform ..."
+  terraform init
+fi
 
 # appliquer la creation
+echo "Application de la création terraform ..."
 terraform apply
 
 # Vérifier si Ansible est installé
 if ! [ -x "$(command -v ansible)" ]; then
-  echo "Installing Ansible..."
+  echo "Installation Ansible..."
   sudo apt install ansible -y 
 fi
 
 # Generer les fichiers hosts
-echo "Generer le fichier host"
-echo "[wordpress]" > hosts
-echo "${terraform output wp_ip} ansible_user=admin" >> hosts
-echo "[db]" >> hosts
-echo "${terraform output db_ip} ansible_user=admin" >> hosts
+if ! [ -f hosts ]; then
+  echo "Generer le fichier host"
+  echo "[wordpress]" > hosts
+  echo "${terraform output wp_ip} ansible_user=admin" >> hosts
+  echo "[db]" >> hosts
+  echo "${terraform output db_ip} ansible_user=admin" >> hosts
+fi
 
 # Appliquer les playbooks Ansible
 ansible-playbook -i hosts wordpress.yml
