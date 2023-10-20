d46781d7651c54ebf079e20976d49bbc407ead5e
houssamD <houssam.darrab@labom2iformation.fr>
2023-10-20 10:30:00 +0200

    add terraform outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,6 +63,7 @@ resource "google_compute_instance" "db" {
     network = google_compute_network.vpc_network.self_link
     subnetwork = google_compute_subnetwork.subnet.self_link 
     access_config {
+      nat_ip = google_compute_address.static.address
     }
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-output "wp_ip" {
-  value = google_compute_address.wp_ip.address
-}
-
 output "wp_intern_ip" {
-  value = google_compute_instance.wp.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.wp.network_interface[0]
 }
 
 output "db_intern_ip" {
   value = google_compute_instance.db.network_interface[0].access_config[0].nat_ip
+}
+
+output "wp_ip" {
+  value = google_compute_address.wp_ip.address
 }
