83aa5b69552c8aea87e1aded9b770d681f0d9577
houssamD <houssam.darrab@labom2iformation.fr>
2023-10-20 10:08:49 +0200

    add terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,10 @@ output "wp_ip" {
   value = google_compute_address.wp_ip.address
 }
 
-output "wp_intern_ip" {
+output "wp" {
   value = google_compute_instance.wp.network_interface[0].access_config[0].nat_ip
 }
 
-output "wp_intern_db" {
+output "db" {
   value = google_compute_instance.db.network_interface[0].access_config[0].nat_ip
 }
