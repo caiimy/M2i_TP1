138d937ba2b357970c19fbc71f09dd934076e7b0
houssamD <houssam.darrab@labom2iformation.fr>
2023-10-24 15:53:17 +0200

    new wordpress files installation

--- deployment.sh
+++ deployment.sh
@@ -62,6 +62,9 @@ cd ..
 # ansible-galaxy install geerlingguy.php
 # ansible-galaxy install geerlingguy.apache
 # ansible-galaxy install geerlingguy.mysql
+sed -i "s/http_host: .*/http_host: $IP_WP_EXT/" ansible/vars/default.yml
+sed -i "s/http_conf .*/http_host: $IP_WP_EXT.conf/" ansible/vars/default.yml
+
 
 # Appliquer les playbooks Ansible
 ansible-playbook -i inventories/hosts playbook/wordpress.yml
